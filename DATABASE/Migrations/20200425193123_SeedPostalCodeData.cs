d1d4b5e3b76a44d0a9df163626e8e8af1fa150a2
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-25 21:33:54 +0200

    Add SeedPostalCodeData

---
+++ DATABASE/Migrations/20200425193123_SeedPostalCodeData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NotTaxTim.Database.Migrations
+{
+    public partial class SeedPostalCodeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PostalCodes",
+                columns: new[] {"Code"},
+                values: new object[] {"7441"}
+            );
+
+            migrationBuilder.InsertData(
+                table: "PostalCodes",
+                columns: new[] {"Code"},
+                values: new object[] {"A100"}
+            );
+
+            migrationBuilder.InsertData(
+                table: "PostalCodes",
+                columns: new[] {"Code"},
+                values: new object[] {"7000"}
+            );
+
+            migrationBuilder.InsertData(
+                table: "PostalCodes",
+                columns: new[] {"Code"},
+                values: new object[] {"1000"}
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
