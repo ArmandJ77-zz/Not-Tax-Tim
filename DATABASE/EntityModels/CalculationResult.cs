3868d2eb896fe6d539d680bfb2b9669f625837b2
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-25 21:04:04 +0200

    Update CalculationResult.cs

--- DATABASE/EntityModels/CalculationResult.cs
+++ DATABASE/EntityModels/CalculationResult.cs
@@ -1,7 +1,17 @@
-﻿namespace NotTaxTim.Database.EntityModels
+﻿using System;
+
+namespace NotTaxTim.Database.EntityModels
 {
     public class CalculationResult
     {
+        public long Id { get; set; }
+        public long PostalCodeId { get; set; }
+        public long TaxCalculationType { get; set; }
+        public decimal AnnualIncome { get; set; }
+        public decimal TotalTax { get; set; }
+        public DateTime DateCreated { get; set; }
 
+        public PostalCode PostalCode { get; set; }
+        public TaxCalculationType CalculationType { get; set; }
     }
 }
