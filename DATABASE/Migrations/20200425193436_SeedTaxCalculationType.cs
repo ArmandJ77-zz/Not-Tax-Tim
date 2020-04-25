2fd7fc0629c110147ae6030e800e9be2aa147bc1
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-25 21:38:51 +0200

    Add SeedTaxCalculationType

---
+++ DATABASE/Migrations/20200425193436_SeedTaxCalculationType.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NotTaxTim.Database.Migrations
+{
+    public partial class SeedTaxCalculationType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TaxCalculationTypes",
+                columns: new[] {"CalculationType"},
+                values: new object[] {"Progressive"}
+            );
+
+            migrationBuilder.InsertData(
+                table: "TaxCalculationTypes",
+                columns: new[] {"CalculationType"},
+                values: new object[] {"Flat Value"}
+            );
+
+            migrationBuilder.InsertData(
+                table: "TaxCalculationTypes",
+                columns: new[] {"CalculationType"},
+                values: new object[] {"Flat Rate"}
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
