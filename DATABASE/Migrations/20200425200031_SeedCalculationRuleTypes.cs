465d5f86cb224815384f843d1960e19bac0c403e
armandjordaan@live.co.za <armandjordaan@live.co.za>
2020-04-25 22:25:56 +0200

    Add SeedCalculationRuleTypes migration

---
+++ DATABASE/Migrations/20200425200031_SeedCalculationRuleTypes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NotTaxTim.Database.Migrations
+{
+    public partial class SeedCalculationRuleTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CalculationRuleTypes",
+                columns: new[] {"PostalCodeId","TaxCalculationTypeId"},
+                values: new object[] {1,1}
+            );
+
+            migrationBuilder.InsertData(
+                table: "CalculationRuleTypes",
+                columns: new[] {"PostalCodeId","TaxCalculationTypeId"},
+                values: new object[] {2,2}
+            );
+
+            migrationBuilder.InsertData(
+                table: "CalculationRuleTypes",
+                columns: new[] {"PostalCodeId","TaxCalculationTypeId"},
+                values: new object[] {3,3}
+            );
+
+            migrationBuilder.InsertData(
+                table: "CalculationRuleTypes",
+                columns: new[] {"PostalCodeId","TaxCalculationTypeId"},
+                values: new object[] {4,1}
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
